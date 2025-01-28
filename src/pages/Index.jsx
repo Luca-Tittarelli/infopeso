056bc3564e3cc0c1ac0be966e3169a267a7fdfbb
Luca Tittarelli <lucatittarelli06@gmail.com>
2025-01-28 08:46:50 -0300

    Add anim for image

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -270,7 +270,12 @@ export default function Index() {
                     Infopeso surgió de la pasión por unir dos mundos fascinantes: la programación y las finanzas. Lo que comenzó como un proyecto personal impulsado por la curiosidad y el entusiasmo, ha evolucionado hasta convertirse en una plataforma innovadora que continúa expandiendo sus horizontes. Con un crecimiento constante y una visión clara.
                     </p>
                 </div>
-                <img src="banner.png" alt="" className="rounded-xl w-full"/>
+                <img src="banner.png" alt="" 
+                    className="rounded-xl w-full"
+                    data-aos="fade-up"
+                    data-aos-duration="700"
+                    data-aos-delay="900"
+                />
                 <a href="https://x.com/infopeso" 
                     className="flex items-center justify-center text-xl font-semibold px-4 py-2 w-fit border rounded-lg shadow-md bg-white text-slate-800 border-slate-700 dark:bg-slate-900 dark:text-slate-300 dark:border-slate-600 hover:bg-slate-100 dark:hover:bg-slate-700 transition-all m-auto mt-10">
                     Seguinos en
